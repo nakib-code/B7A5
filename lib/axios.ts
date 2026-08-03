ac9517ceb8290ffcaf74e9c9a07ff7e610f56e81
Ahmed Nakib <office.nakib@gmail.com>
2026-08-03 10:29:04 -0700

    try to bug fix

--- lib/axios.ts
+++ lib/axios.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
+
 export const axiosInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  baseURL: "https://fixitnow-backend-seven.vercel.app/api",
   withCredentials: true,
 });
